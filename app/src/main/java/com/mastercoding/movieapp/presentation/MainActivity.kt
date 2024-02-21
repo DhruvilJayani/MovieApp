671d1884c96ed64246e4dfb73a14073b803e4ddd
Dhuula <dhruvil.j@wellnest.tech>
2024-02-21 18:46:20 +0530

    Removing Commit

--- app/src/main/java/com/mastercoding/movieapp/presentation/MainActivity.kt
+++ app/src/main/java/com/mastercoding/movieapp/presentation/MainActivity.kt
@@ -25,7 +25,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var adapter: MovieAdapter
 
-    //Checking GitHub
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
